a103443473c82e5da0ac38dc2c6687c8e4710910
Venkat <vraju4865@gmail.com>
2023-08-12 18:53:58 +0530

    basic grid design

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const size = prompt("Enter size of grid",16);
-console.log(size * size);
+const gridWidth = 365 / (size* size);
+console.log(gridWidth);
 
 let container = document.querySelector('#container');
+const eraser = document.querySelector('#eraser');
+const paint = document.querySelector('#paintbtn');
+
+
 
 for( let i = 0;i < size * size; i++){
     const newDiv = document.createElement('div');
-    // newDiv.setAttribute('draggable',true);
+    
     newDiv.classList.add('grid');
     container.appendChild(newDiv);
+    const divCombined = document.querySelectorAll('.grid');
+    const grid = document.querySelector('.grid');
+    
+    
+    function paintFunc(){
+        divCombined.forEach((grid) => {
+            grid.addEventListener('mouseenter',()=>{
+                grid.style.cssText = 'background-color: yellow';
+            })
+        })
+    }
 
-    newDiv.addEventListener('hover',()=>{
-        newDiv.style.backgroundColor = 'red';
+    function eraserFunc(){
+        divCombined.forEach((grid) => {
+            grid.addEventListener('mouseenter',()=>{
+                grid.style.cssText = 'background-color: rgb(224, 224, 233)'
+            })
+        })
+    }
+
+    divCombined.forEach((grid) => {
+        grid.style.width = `${gridWidth}`;
     })
+    
+    eraser.addEventListener('click',eraserFunc);
+    paint.addEventListener('click',paintFunc);
 }
+
+
 
